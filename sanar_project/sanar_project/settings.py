741f4c2c70e7991128d1d4033654e02e6501eeab
Vinit Dhull <vlovewebsolutions@gmail.com>
2022-06-09 11:18:58 +0000

    Updated some changes for making responsive

--- sanar_project/sanar_project/settings.py
+++ sanar_project/sanar_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z^$kis!#!fg8q86$9lk)g^99r*l6+$=fhj*)yq)^pzz!!u%b2#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
